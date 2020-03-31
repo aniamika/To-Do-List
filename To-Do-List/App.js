df2009d5a3b1783a1e5eb5ae013a8d1e9ab532ae
Anna Mika <aniamika90@gmail.com>
2020-04-01 00:31:45 +0200

    add basic styling of navigation pages

--- To-Do-List/App.js
+++ To-Do-List/App.js
@@ -1,37 +1,38 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function DetailsScreen() {
+function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
-    </View>
-  );
-}
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.home}>
       <Text>Home screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
     </View>
   );
 }
 
-function SettingsScreen({ navigation }) {
+function TasksListScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+    <View style={styles.tasksList}>
+      <Text>Tasks List screen</Text>
+      <Button title="Add Task +" onPress={() => navigation.navigate('NewTask')}/>
+    </View>
+  );
+}
+
+function LastScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Last screen</Text>
+    </View>
+  );
+}
+
+function NewTaskScreen() {
+  return (
+    <View style={styles.newTask}>
+      <Text>Create new task</Text>
     </View>
   );
 }
@@ -42,19 +43,26 @@ function HomeStackScreen() {
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
     </HomeStack.Navigator>
   );
 }
 
-const SettingsStack = createStackNavigator();
+const TasksListStack = createStackNavigator();
 
-function SettingsStackScreen() {
+function TasksListStackScreen() {
   return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
+    <TasksListStack.Navigator>
+      <TasksListStack.Screen name="TasksList" component={TasksListScreen} />
+      <TasksListStack.Screen name="NewTask" component={NewTaskScreen} />
+    </TasksListStack.Navigator>
+  );
+}
+
+function LastScreenStackScreen() {
+  return (
+    <LastScreenStack.Navigator>
+      <LastScreenStack.Screen name="LastScreen" component={LastScreen} />
+    </LastScreenStack.Navigator>
   );
 }
 
@@ -64,9 +72,36 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen}/>
+        <Tab.Screen name="TasksList" component={TasksListStackScreen} />
+        <Tab.Screen name="LastScreen" component={LastScreenStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  home: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: 'grey',
+  },
+  tasksList: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: 'beige',
+  },
+  newTask: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: 'pink',
+  },
+  addButton: {
+    borderRadius: 50,
+    color: 'red',
+    backgroundColor: '#f9c2ff',
+  },
+})
